d4d01a5e69574aac233872e3002e3625154de571
Joao Couto <joaom.couto55@gmail.com>
2019-04-13 13:07:45 -0300

    Final fully functional

--- main.c
+++ main.c
@@ -2,8 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-
-
 typedef struct 
 {
     //int id;
@@ -24,7 +22,7 @@ typedef struct
     int * currentMatch ; //vetor com estudantes alocados pra universidade
 } University;
 
-/*
+
 int existsProposal(Student * student, int numStudents){  //Dado o conjunto de estudantes, retorna o id do primeiro estudante sem uni e que nao propos pra todas, 0 se nao existir
     int i ;
     for (i = 0; i < numStudents; i++ ){
@@ -52,7 +50,7 @@ int uniPrefers(University university, Student * student, int proposingStudent){ 
     }
     return 0 ;
 }
-*/
+
 int main( int argc, char *argv[] )
 {
     // ------------------------------------------------- LEITURA 
@@ -76,13 +74,6 @@ int main( int argc, char *argv[] )
             university[i].currentMatch[j] = -1 ;
         }
     }
-//---TESTE LEITURA UNIs
-    for(i = 0; i < numUniversities; i++){
-        printf("%i %i ",university[i].slots, university[i].score) ;
-    }
-//---ENDTEST
-
-
     for(i = 0; i < numStudents; i++){ 
         fscanf(students, "%i %i", &student[i].appNum, &student[i].score) ; 
         student[i].preference = malloc(student[i].appNum*sizeof(int)) ;
@@ -94,54 +85,42 @@ int main( int argc, char *argv[] )
     }
     //---------------------------------------------- END LEITURA
 
-//--------TESTE LEITURA STUDENTS
-    for(i = 0; i < numStudents; i++){ 
-        printf("%i %i ", student[i].appNum, student[i].score) ; 
-        for (j = 0 ; j < student[i].appNum ; j++){
-            printf("%i ", student[i].preference[j]) ; 
-        }
-    }
-//---------END TEST
-
-
 int proposingStudent ;
 int proposedUniversity ;
 int freeUniSlot ;
 int k ;
 int droppingStudent ;
 int droppingMatch;
-/*
+
 while(i = existsProposal(student, numStudents)){ //Retorna o ID do proximo estudante a propor
     proposingStudent = i -1 ; //Indice REAL do studante que vai propor
     proposedUniversity = student[proposingStudent].preference[student[proposingStudent].proposalCount] - 1 ; //Index REAL da uni que vai receber a proposta[]
     student[proposingStudent].proposalCount += 1 ;
-    if (j = existsSlot(university[proposedUniversity])){ //Dada a uni que vai receber a proposta, se ela tiver vaga, retorna o ID da vaga
-        freeUniSlot = j - 1 ; //Indice REAL da vaga vazia dentro do currentMatch
-        university[proposedUniversity].currentMatch[freeUniSlot] = proposingStudent ;
-        student[proposingStudent].uni = proposedUniversity ;
-    } else if(k = uniPrefers(university[proposedUniversity], student, proposingStudent)) { //Dada a uni que recebera a proposta, ja lotada, retorna o ID da vaga ocupada por um aluno que prefere menos
-        droppingMatch = k - 1 ; //index real da vaga que sera substituida
-        droppingStudent = university[proposedUniversity].currentMatch[droppingMatch] ; 
-        student[droppingStudent].uni = - 1;
-        student[proposingStudent].uni = proposedUniversity ;
-        university[proposedUniversity].currentMatch[droppingMatch] = proposingStudent ;
+    if(student[proposingStudent].score >= university[proposedUniversity].score){
+        if (j = existsSlot(university[proposedUniversity])){ //Dada a uni que vai receber a proposta, se ela tiver vaga, retorna o ID da vaga
+            freeUniSlot = j - 1 ; //Indice REAL da vaga vazia dentro do currentMatch
+            university[proposedUniversity].currentMatch[freeUniSlot] = proposingStudent ;
+            student[proposingStudent].uni = proposedUniversity ;
+        } else if(k = uniPrefers(university[proposedUniversity], student, proposingStudent)) { //Dada a uni que recebera a proposta, ja lotada, retorna o ID da vaga ocupada por um aluno que prefere menos
+            droppingMatch = k - 1 ; //index real da vaga que sera substituida
+            droppingStudent = university[proposedUniversity].currentMatch[droppingMatch] ; 
+            student[droppingStudent].uni = - 1;
+            student[proposingStudent].uni = proposedUniversity ;
+            university[proposedUniversity].currentMatch[droppingMatch] = proposingStudent ;
+        }
     }
-
 }
-*/
 
-/*
+
+printf("Grupos com alocacao\n") ;
 for( i = 0; i < numStudents; i++){
-    printf("%i %i ", i , student[i].uni) ;
+    
+    if(student[i].uni != -1) printf("%i %i\n", i+1 , student[i].uni +1) ;
 }
-*/
-
-
+printf("Candidatos nao alocados\n") ;
+for( i = 0; i < numStudents; i++){
     
-    
-
-
-
-
+    if(student[i].uni == -1) printf("%i\n", i+1) ;
+}
    return 0 ;
 }
