1330968f8410349f2284cd53d46da8b66ccaa8a9
Joao Couto <joaom.couto55@gmail.com>
2019-04-13 15:38:17 -0300

    Modularizacao inicial

--- main.c
+++ main.c
@@ -4,7 +4,6 @@
 
 typedef struct 
 {
-    //int id;
     int score;
     int appNum ;
     int uni ;
@@ -15,8 +14,6 @@ typedef struct
 
 typedef struct
 {
-    //int id ;
-    //int * preference ;
     int slots ;
     int score ;
     int * currentMatch ; //vetor com estudantes alocados pra universidade
@@ -56,7 +53,6 @@ int uniPrefers(University university, Student * student, int proposingStudent){ 
         }
     }
 
-    
     if(student[proposingStudent].score > student[university.currentMatch[minIndex]].score){ //Se o score do proposing for maior que o score do menor current, troca
         return minIndex + 1;
     } else if(student[proposingStudent].score == student[university.currentMatch[minIndex]].score){ //Se o score deles for igual, vai pegar o que tiver menor preferencia pela uni
@@ -68,48 +64,40 @@ int uniPrefers(University university, Student * student, int proposingStudent){ 
     return 0 ;
 }
 
-int main( int argc, char *argv[] )
-{
-    // ------------------------------------------------- LEITURA 
-    int numUniversities ;
-    int numStudents ;
-    FILE *universities ;
-    FILE *students ;
-    universities = fopen(argv[1], "r") ;
-    students = fopen(argv[2], "r") ; 
-    fscanf (universities, "%i", &numUniversities) ;
-    fscanf (students, "%i", &numStudents) ;
-    Student student[numStudents];
-    University university[numUniversities] ;
+int initializeUniversities(FILE * universities, int numUniversities, University * university ){
     int i,j ;
-
     for(i = 0; i < numUniversities; i++){
-       // university[i].id = i + 1 ;
+        // university[i].id = i + 1 ;
         fscanf(universities,"%i %i",&university[i].slots, &university[i].score) ;
         university[i].currentMatch = malloc(university[i].slots*sizeof(int)) ;
         for(j = 0; j < university[i].slots ; j++){
-            university[i].currentMatch[j] = -1 ;
+             university[i].currentMatch[j] = -1 ;
         }
     }
+    return 0;
+ }
+
+ int initializeStudents(FILE * students, int numStudents, Student * student){
+    int i,j ;
     for(i = 0; i < numStudents; i++){ 
         fscanf(students, "%i %i", &student[i].appNum, &student[i].score) ; 
         student[i].preference = malloc(student[i].appNum*sizeof(int)) ;
         for (j = 0 ; j < student[i].appNum ; j++){
             fscanf(students, "%i", &student[i].preference[j]) ; 
         }
-        student[i].uni = -1 ;
-        student[i].proposalCount = 0 ; 
+    student[i].uni = -1 ;
+    student[i].proposalCount = 0 ; 
     }
-    //---------------------------------------------- END LEITURA
+ }
 
-int proposingStudent ;
-int proposedUniversity ;
-int freeUniSlot ;
-int k ;
-int droppingStudent ;
-int droppingMatch;
-
-while(i = existsProposal(student, numStudents)){ //Retorna o ID do proximo estudante a propor
+ int stableMarriage(Student * student, University * university, int numStudents, int numUniversities){
+    int proposingStudent ;
+    int proposedUniversity ;
+    int freeUniSlot ;
+    int i,j,k ;
+    int droppingStudent ;
+    int droppingMatch;
+    while(i = existsProposal(student, numStudents)){ //Retorna o ID do proximo estudante a propor
     proposingStudent = i -1 ; //Indice REAL do studante que vai propor
     proposedUniversity = student[proposingStudent].preference[student[proposingStudent].proposalCount] - 1 ; //Index REAL da uni que vai receber a proposta[]
     student[proposingStudent].proposalCount += 1 ;
@@ -126,18 +114,34 @@ while(i = existsProposal(student, numStudents)){ //Retorna o ID do proximo estud
             university[proposedUniversity].currentMatch[droppingMatch] = proposingStudent ;
         }
     }
+    }
 }
 
-
-printf("Grupos com alocacao\n") ;
-for( i = 0; i < numStudents; i++){
-    
-    if(student[i].uni != -1) printf("%i %i\n", i+1 , student[i].uni +1) ;
-}
-printf("Candidatos nao alocados\n") ;
-for( i = 0; i < numStudents; i++){
-    
-    if(student[i].uni == -1) printf("%i\n", i+1) ;
-}
-   return 0 ;
+int main( int argc, char *argv[] )
+{
+    int numUniversities ;
+    int numStudents ;
+    FILE *universities ;
+    FILE *students ;
+    universities = fopen(argv[1], "r") ;
+    students = fopen(argv[2], "r") ; 
+    fscanf (universities, "%i", &numUniversities) ;
+    fscanf (students, "%i", &numStudents) ;
+    Student student[numStudents];
+    University university[numUniversities] ;
+    int i,j ;
+    initializeUniversities(universities,numUniversities ,university ) ;
+    initializeStudents(students,numStudents, student);
+    stableMarriage(student, university, numStudents, numUniversities) ;
+    printf("Grupos com alocacao\n") ;
+    for( i = 0; i < numStudents; i++){
+        
+        if(student[i].uni != -1) printf("%i %i\n", i+1 , student[i].uni +1) ;
+    }
+    printf("Candidatos nao alocados\n") ;
+    for( i = 0; i < numStudents; i++){
+        
+        if(student[i].uni == -1) printf("%i\n", i+1) ;
+    }
+    return 0 ;
 }
